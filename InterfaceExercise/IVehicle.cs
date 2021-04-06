6100a6df684a5f1d5e1db7b8499006fd301179e5
Key343 <keyonna.sharde@gmail.com>
2021-04-05 21:23:21 -0400

    initial commit

---
+++ InterfaceExercise/IVehicle.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace InterfaceExercise
+{
+    public interface IVehicle
+    {
+    }
+
+    public object MyProperty { get; set; }
+    public object MyProperty { get; set; }
+    public object MyProperty { get; set; }
+}
